b5d3afacf4e3e04a2c717e514c8f06c960c17804
Julian <Rijkenjulian@gmail.com>
2019-03-25 09:26:23 +0100

    Vrij kut

--- Assets/Oculus/VR/Scripts/Util/TableHandler.cs
+++ Assets/Oculus/VR/Scripts/Util/TableHandler.cs
@@ -1,20 +1,4 @@
-﻿/************************************************************************************
-Copyright : Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
-
-Licensed under the Oculus Utilities SDK License Version 1.31 (the "License"); you may not use
-the Utilities SDK except in compliance with the License, which is provided at the time of installation
-or download, or which otherwise accompanies this software in either electronic or hard copy form.
-
-You may obtain a copy of the License at
-https://developer.oculus.com/licenses/utilities-1.31
-
-Unless required by applicable law or agreed to in writing, the Utilities SDK distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-ANY KIND, either express or implied. See the License for the specific language governing
-permissions and limitations under the License.
-************************************************************************************/
-
-using System;
+﻿using System;
 using UnityEngine;
 
 public class TableHandler : MonoBehaviour
@@ -121,14 +105,15 @@ public class TableHandler : MonoBehaviour
     /// </summary>
     virtual public void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
     {
+        AddAngularVelocity(grabbedRigidbody.velocity.magnitude);
+
+
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         rb.isKinematic = m_grabbedKinematic;
         rb.velocity = linearVelocity;
         rb.angularVelocity = angularVelocity;
         m_grabbedBy = null;
         m_grabbedCollider = null;
-
-        AddAngularVelocity(grabbedRigidbody.velocity.magnitude);
     }
 
     void Awake()
@@ -149,9 +134,7 @@ public class TableHandler : MonoBehaviour
 
     protected virtual void Start()
     {
-        m_grabbedKinematic = GetComponent<Rigidbody>().isKinematic;
-
-       
+        m_grabbedKinematic = GetComponent<Rigidbody>().isKinematic;     
     }
 
 
